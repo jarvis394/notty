c6509e6906e56e94ea0e765573c6a6d0ee6f00d1
jarvis394 <tarnatovski@gmail.com>
2020-02-26 14:38:37 +0300

    feature: --no-window for list command

--- notty/app.py
+++ notty/app.py
@@ -21,9 +21,14 @@ def get_ids(ctx, args, incomplete):
 
 @click.group(cls=ClickAliasedGroup)
 def cli():
+    """
+    Fast terminal-based notes application
+
+    Storage is located in the system's default app_dir. SQLite3 is used to store and process your notes.
+    """
     pass
 
-@cli.command(aliases=['edit', 'e'], help='edits a note in your default editor')
+@cli.command(aliases=['edit', 'e'], help='Edits a note in your default editor')
 @click.argument('id', type=click.INT, autocompletion=get_ids)
 @click.option('--editor', '-e')
 def edit(id, editor):
@@ -40,13 +45,29 @@ def edit(id, editor):
     db.update_text(id, new_text)
     click.echo(f"\n\n  Note with ID {Fore.YELLOW}{id}{RS} was successfully saved!\n")
 
-@cli.command(aliases=['create', 'c'], help='creates a new note')
+@cli.command(aliases=['create', 'c'], help='Creates a new note')
 def create():
     return screens.create.execute()
 
-@cli.command(aliases=['list', 'l'], help='lists your notes in a cool fancy window')
-def list():
-    return screens.list.execute()
+@cli.command(aliases=['list', 'l'], help='Lists your notes in a cool fancy window')
+@click.argument('id', type=int, required=False, default=None)
+@click.option('--no-window', '-n', default=False, is_flag=True, help='print your notes as a list without showing a fancy window')
+def list(no_window, id):
+    if no_window or id:
+        def format_note(note):
+            id, title, text, ts = note["id"], note["title"], note["text"], note["ts"]
+            formatted_text = '\n    '.join(text.strip().split('\n'))[:96] + ('...' if len(text) > 96 else '')
+            return '\n'.join([
+                f"{Fore.YELLOW}[{id}]{RS} {Style.BRIGHT}{Fore.CYAN}{title}{RS}",
+                f"{Style.DIM}Date: {ts}{RS}",
+                f"\n    {formatted_text}\n\n"
+            ])
+
+        notes = reversed(db.get_all())
+        notes = map(format_note, notes)
+        click.echo_via_pager(notes)
+    else:
+        return screens.list.execute()
 
 # Initialize color support
 colorama.init()
